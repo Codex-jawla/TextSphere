50a4869e4c69308929d7e456c380285d29b79885
HIMANSHU JAWLA <101585686+Codex-jawla@users.noreply.github.com>
2023-10-28 13:16:56 +0530

    Update App.js

--- TextSphere/src/App.js
+++ TextSphere/src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
 import Textsphere from './components/Testsphere';
-import Particle from './components/Particle';
 
 function App() {
   return (
     <>
-    <Particle/>
       <Textsphere/>
     </>
   );
